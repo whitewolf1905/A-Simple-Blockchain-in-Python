5a7bf5471a51f2aab074113fc580b7c879bfc30e
whitewolf1905 <b17086@students.iitmandi.ac.in>
2020-11-19 22:02:21 +0530

    Updated Block.py

--- Block.py
+++ Block.py
@@ -5,6 +5,6 @@ class Block:
 	def __init__(self, previous_hash, transaction):
 		self.previous_hash = previous_hash
 		self.transaction = transaction
-		current_hash_string = "".join(transaction) + previous_hash
+		current_hash_string = "".join(transaction)
 		current_hash = hashlib.sha256(current_hash_string.encode()).hexdigest()
 		self.current_hash = current_hash 
